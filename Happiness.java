58d3c67838c2fffbb4ae399f0ea4544113e3adf9
SergeyPonomarenko92 <35901988+SergeyPonomarenko92@users.noreply.github.com>
2020-02-19 20:29:00 +0200

    Random numbers

---
+++ Happiness.java
@@ -0,0 +1,31 @@
+import  java.util.Random;
+import  java.util.Scanner;
+public class Happiness {
+    public static void main(String[] args) {
+    Random random = new Random();
+    Scanner enter = new Scanner(System.in);
+
+    int Happiness, Luck, Wealth, Health, Intelligence, Age;
+    Happiness = 0;
+    Luck = 0;
+    Wealth= 0;
+    Health = 0;
+    Intelligence = 0;
+        System.out.println("Please enter your age");
+        Age =  enter.nextInt();
+
+        while(Age < 100){
+            Happiness += random.nextInt(100);
+            Luck += random.nextInt(100);
+            Wealth += random.nextInt(100);
+            Health += random.nextInt(100);
+            Intelligence += random.nextInt(100);
+            ++Age;
+            System.out.println("In your " + Age + " years, your a fucking good");
+            System.out.println("You Happiness level is " + Happiness + " !!!");
+            System.out.println("You Luck percent bar is " + Luck + "% !!!");
+            System.out.println("You wealth level is " + Wealth + " !!!");
+            System.out.println("You Intelligence level is " + Intelligence + " of 100 !!!" );
+        }
+    }
+}
